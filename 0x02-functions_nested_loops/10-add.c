67e8b9fda846b6e8703ac13aa5f903abda204061
nokwadaazondi <nokwandaazondi@gmail.com>
2023-07-19 17:32:28 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ *
+ * @x: number belong added
+ *
+ * @y:number belong added
+ *
+ * Return: Always 0.
+ *
+ */
+int add(int x, int y)
+{
+
+	return (x * y);
+}
