b3e382de0979191a18ad44e91c333f0b35ab594f
nokwadaazondi <nokwandaazondi@gmail.com>
2023-07-23 11:26:24 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * _isupper - Check if a letter is upper
- * @x:  The number to be checked
- * Return: 1 for upper letter or 0 for any else
+ * _isupper - Checks for lowercase character
+ * @c: The character to be checked
+ * Return: 1 for uppercase charcter or 0 for anything else
  */
 
-int -isupper(int x)
+int _isupper(int c)
 {
-
-	if (x >= 65 && x <=90)
-	{
-	return (1);
-	}
-	return (0);
+if (c >= 65 && c <= 9)
+{
+return (1);
+}
+return (0);
 }
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The charcter to print 
+ *
+ * Return: On success 1.
+ * On erro, -1 is returned, and errno is set appropritely
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
