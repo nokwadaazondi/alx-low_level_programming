9d32bbc73830193a13017dc886452c804f963cc7
nokwadaazondi <nokwandaazondi@gmail.com>
2023-07-23 04:38:09 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * _isupper - Check if a letter is upper
- * @x: The number to be checked
+ * @x:  The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
 
-int _isupper(int x)
+int -isupper(int x)
 {
-	if (x >= 65 && x <= 90)
+	if (x >= 65 && x <=90)
 	{
 	return (1);
 	}
