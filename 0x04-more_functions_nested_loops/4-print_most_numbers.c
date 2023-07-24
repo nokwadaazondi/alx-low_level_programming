42c14d4a5da722b1444c9282b257bfbb91196e48
nokwadaazondi <nokwandaazondi@gmail.com>
2023-07-23 21:04:20 -0700

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,14 @@
 
 void print_most_numbers(void)
 {
-	char c;
+	int num;
 
-	for (c = '0'; c <= '9'; c++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0');
+		}
 	}
 	_putchar('\n');
 }
