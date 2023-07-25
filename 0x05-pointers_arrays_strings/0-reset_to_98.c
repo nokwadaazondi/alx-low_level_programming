bc1a08e123e169c513a5963926947f6f46b4d3ed
nokwadaazondi <nokwandaazondi@gmail.com>
2023-07-25 09:53:23 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * rest_to_98 - a function that takes a pointet to an int as parameter
+ * and updates the value it points to do 98
+ * @n: input
+ * Return: n
+ */
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
