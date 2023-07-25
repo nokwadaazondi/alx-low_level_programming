694b9014df8607a0829e1dc08010345274981307
nokwadaazondi <nokwandaazondi@gmail.com>
2023-07-25 16:33:56 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,7 +4,7 @@
 /**
  * print_array - prints n elements of an array
  * @a: array
- * @n: numbers\ of values to be printed
+ * @n: number of values to be printed
  */
 void print_array(int *a, int n)
 {
