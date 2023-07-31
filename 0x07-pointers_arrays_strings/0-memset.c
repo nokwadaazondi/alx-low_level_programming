81846c50c7ac87cef311de33179f863422f26cca
nokwadaazondi <nokwandaazondi@gmail.com>
2023-07-31 06:49:42 -0700

    0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (Success)
+ */
+char *_mamset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (;n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
