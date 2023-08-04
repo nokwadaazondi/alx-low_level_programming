d283c526c004096c246ac5f010f3eb240fcfa37d
nokwadaazondi <nokwandaazondi@gmail.com>
2023-08-04 05:23:32 -0700

    pow_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * power operator - return the natural square root of a number
+ * @n:input number
+ * @c: iterator
+ * Rreturn: square root or -1
+ */
+int sqrt_q(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > 4)
+	{
+		return (-1);
+	}
+	return  (sqrt_a(a, b + 1);
+}
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: the input number
+ * Return: natural square root
+ */
+int _sqrt_recursion(int n)
+{
+return (sqrt(n, 0));
+}
