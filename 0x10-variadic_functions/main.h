a90bd19c51ccfac69871e230c960b472c572ac05
nokwadaazondi <nokwandaazondi@gmail.com>
2023-08-17 09:49:58 -0700

    Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/main.h
+++ 0x10-variadic_functions/main.h
@@ -1,9 +1,6 @@
 #ifndef VAR_FUNC_H_
 #define VAR_FUNC_H_
 
-#include <stdio.h>
-#include <stdarg.h>
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,10 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIABLE_FUNCTIONS_H
 
+#include <stdio.h>
+#include <stdarg.h>
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
